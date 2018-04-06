c23ef39db17a0c25839160d24a080e3006cdac2b
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2018-04-06 12:12:20 +0200

    Switch to explicit index management
    With this commit we switch all tracks to explicit index management
    (instead of index auto-management). As this change is breaking, we also
    increase the track version from 1 to 2 thus requiring at least Rally
    0.9.0 to use these tracks.
    This allows us to also remove some legacy codd that has registered the
    `refresh` operation for older versions of Rally (newer versions already
    include this operation in Rally core).
    
    Relates elastic/rally#380
    Closes #37
    Relates #41

--- percolator/track.py
+++ percolator/track.py
@@ -7,15 +7,5 @@ def percolate(es, params):
     return 1, "ops"
 
 
-def refresh(es, params):
-    es.indices.refresh(index=params.get("index", "_all"))
-
-
 def register(registry):
-    try:
-        major, minor, patch, _ = registry.meta_data["rally_version"]
-    except AttributeError:
-        # We must be below Rally 0.8.2 (did not provide version metadata).
-        # register "refresh" for older versions of Rally. Newer versions have support out of the box.
-        registry.register_runner("refresh", refresh)
     registry.register_runner("percolate", percolate)

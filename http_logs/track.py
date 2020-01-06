c86dcc2ee620ed2b6e06e45da817aa69bb5f496e
Henning Andersen <henning.andersen@elastic.co>
2020-01-06 15:54:39 +0100

    Only add explicit challenge
    No longer run reindex as part of the default challenge,
    since it takes too long.
    
    Changed reindex to report back docs/s instead of ops/s

--- http_logs/track.py
+++ http_logs/track.py
@@ -1,5 +1,6 @@
 def reindex(es, params):
-  es.reindex(body=params.get("body"), request_timeout=params.get("request_timeout"))
+  result = es.reindex(body=params.get("body"), request_timeout=params.get("request_timeout"))
+  return result["total"], "docs"
 
 def register(registry):
   registry.register_runner("reindex", reindex)

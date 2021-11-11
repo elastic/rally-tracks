48e9487215fe2f6f0f4d8a4cf1e435c8c97e8236
Mayya Sharipova <mayya.sharipova@elastic.co>
2021-11-11 15:31:48 -0500

    Address Dimitrios' comments 1

--- vectors_deep1b/_tools/parse.py
+++ vectors_deep1b/_tools/parse.py
@@ -1,10 +1,21 @@
+#!/usr/bin/env python3
 import sys
 import struct
 import json
-from tqdm import tqdm
+
+try:
+	from tqdm import tqdm
+	iterate = lambda i: tqdm(range(i))
+except ModuleNotFoundError:
+	print("Warning: [tqdm] package is not available and you won't be able to see progress.", file=sys.stderr)
+	iterate = range
+
+dims = 96
+num_vectors = 9990000 
+
 
 def to_json(f):
-	for i in tqdm(range(num_vectors)) :
+	for i in iterate(num_vectors):
 		f.read(4)  # in .fvecs format for each vector the first 4 bytes represent dim
 		vector = struct.unpack('f' * dims, f.read(dims * 4))
 
@@ -13,8 +24,9 @@ def to_json(f):
 		print(json.dumps(record, ensure_ascii=False))
 
 
-dims = 96
-num_vectors = 9990000 
+if len(sys.argv) != 2:
+	print(f"Error: No .fvecs file. Rerun using [{sys.argv[0]} /path/to/deep10M.fvecs].")
+	sys.exit(1)
 
 with open(sys.argv[1], 'rb') as f:
-	to_json(f)	
+	to_json(f)

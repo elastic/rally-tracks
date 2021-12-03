68bbfd204f5ab4867b403d90b4d3d5eb921f1dba
Luegg <lukas.wegmann@elastic.co>
2021-12-03 14:38:58 +0100

    - add wait-until-merges-finish - fix query config for ES < 7.16

--- noaa/track.py
+++ noaa/track.py
@@ -1,15 +1,9 @@
 
 
 async def sql(es, params):    
-    body = params["body"]
-    # default page_timeout is not suited for benchmarks as it keeps the scroll contexts open for 45s
-    if 'page_timeout' not in body:
-        body['page_timeout'] = '100ms'
-
     await es.sql.query(
-            body=body,
-            request_timeout=params.get("request-timeout")
-        )
+        body=params["body"]
+    )
 
 
 def register(registry):

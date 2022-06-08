08c340b9f4691af13493053adecee13dfe1d6b66
Mike Baamonde <michaelbaamonde@users.noreply.github.com>
2022-06-08 13:30:59 -0400

    Upgrade custom runners to be compatible with elasticsearch-py 8.2.0. (#271)
    * Use kwargs exclusively when calling ES client methods in eql runner.
    
    * ML client methods are no longer namespaced under xpack.

--- eql/track.py
+++ eql/track.py
@@ -5,7 +5,7 @@ async def eql(es, params):
         cluster += ":"
 
     await es.eql.search(
-            cluster + params.get("index"),
+            index=cluster + params.get("index"),
             body=params.get("body"),
             request_timeout=params.get("request-timeout")
         )

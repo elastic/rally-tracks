592651b9fffc8531b73154dd499878e5d10c16c4
Martijn van Groningen <martijn.v.groningen@gmail.com>
2022-06-29 16:03:39 +0200

    Modify tsdb track to allow ingesting into a data stream (#275)
    This change adds an `ingest_mode` track option to the tsdb track. If this track option is set to `data_stream`
    then this track sets up a composable index templates that allows for ingesting into a tsdb data stream.
    
    If the `ingest_mode`track param  isn't set or set to any other value than `data_stream` then it fall back to ingesting into a tsdb index, which is what the track is doing today. By default the `ingest_mode` track option is not specified so ingesting into a tsdb index is the default behaviour. 
    
    Note that the mappings between `index.json` and `index-template.json` are identical. Only the settings
    section differ slightly. In the case for tsdb data stream the following settings get generated upon data stream creation:
    
    - `index.time_series.start_time` - based on current time and the `index.look_ahead_time` index setting
    - `index.time_series.end_time` - based on current time and the `index.look_ahead_time` index setting
    - `index.routing_path` (based from the mapping, essentially all fields with `time_series_dimension=true` attribute)
    
    Another difference between the data stream and index ingest mode is that the `ingest mode=data_stream` also 
    uses a pipeline. This to update the `@timestamp` field of documents to be close to current time. A painless script
    is used to compute the number of days between `2021-04-28T17:18:23.410Z` (first timestamp in the data set) and current time. Then the pipeline adds that number of days to the `@timestamp` of documents, this makes the timestamp close to current time and maintains the original timestamp distribution. The data set roughly contains a days worth of metrics.
    
    Also `dedupe.py` tool is added to the `_tools` directory. When ingesting into a tsdb data stream the `op_type=create` is required and so when a document has the same dimension field values and the same timestamp (dimension fields and timestamp are used to create the _id of a tsdb document) then a version conflict occurs (409). This tool removes the duplicates, so that no 409 are retuning when ingesting the data via the bulk api. Note that when indexing into a tsdb data stream the default `op_type=index` is used and then duplicates just get overwritten and Rally doesn't return an errors, so before it wasn't an issue from a benchmark perspective.

---
+++ tsdb/_tools/dedupe.py
@@ -0,0 +1,111 @@
+#!/usr/bin/env python3
+
+####################################################################
+#
+# A tool that dedupes a sorted anonymized metricbeat dump.
+#
+####################################################################
+#
+# Expects sorted anonymized metricbeat dump as input via standard
+# in and returns a deduped sorted anonymized metric beat output via
+# standard out. Also seperately generates 'dupes-' prefixed files 
+# per metric set name containing the dupes for manual inspection.
+#
+####################################################################
+
+import json
+import sys
+
+def generate_event_key(parsed_line):
+    return parsed_line['kubernetes']['event']['involved_object']['uid']
+
+def generate_state_container_key(parsed_line):
+    key = parsed_line['kubernetes']['container']['name']
+    key += parsed_line['kubernetes']['pod']['name']
+    key += parsed_line['kubernetes']['node']['name']
+    container_id = parsed_line.get('kubernetes',{}).get('container', {}).get('id')
+    if (container_id is not None):
+        key += container_id
+    return key
+
+def generate_state_pod_key(parsed_line):
+    return parsed_line['kubernetes']['pod']['name'] + generate_node_key(parsed_line)
+
+def generate_container_key(parsed_line):
+    return parsed_line['kubernetes']['container']['name'] + parsed_line['kubernetes']['pod']['name'] + generate_node_key(parsed_line)
+
+def generate_volume_key(parsed_line):
+    return parsed_line['kubernetes']['volume']['name'] + parsed_line['kubernetes']['pod']['name'] + generate_node_key(parsed_line)
+
+def generate_pod_key(parsed_line):
+    return parsed_line['kubernetes']['pod']['name'] + generate_node_key(parsed_line)
+
+def generate_node_key(parsed_line):
+    return parsed_line['kubernetes']['node']['name']
+
+def generate_system_key(parsed_line):
+    return generate_node_key(parsed_line) + parsed_line['kubernetes']['system']['container']
+
+def generate_state_node_key(parsed_line):
+    return generate_node_key(parsed_line)
+
+generate_key_functions = {
+    'event': generate_event_key,
+    'state_container': generate_state_container_key,
+    'state_pod': generate_state_pod_key,
+    'container': generate_container_key,
+    'volume': generate_volume_key,
+    'pod': generate_pod_key,
+    'node': generate_node_key,
+    'system': generate_system_key,
+    'state_node': generate_state_node_key
+}
+
+in_count = 0
+error_count = 0
+out_count = 0
+current_timestamp = None
+keys = set()
+
+dupe_files = {}
+
+with open('error_lines.json', 'a') as error_file:
+    for line in sys.stdin:
+        in_count += 1
+        try:
+            parsed = json.loads(line)
+            line_timestamp = parsed['@timestamp']
+            metric_set_name = parsed['metricset']['name']
+            if parsed.get('error') is not None:
+                error_count += 1
+                print(line, file=error_file)
+                continue
+
+            generate_key_function = generate_key_functions[metric_set_name]
+            key = metric_set_name + generate_key_function(parsed)
+            if (current_timestamp == line_timestamp):
+                if key in keys:
+                    dupe_file_name = f"dupes-{metric_set_name}.json"
+                    dupe_file = dupe_files.get(dupe_file_name)
+                    if dupe_file is None:
+                        dupe_file = open(dupe_file_name, 'a')
+                        dupe_files[dupe_file_name] = dupe_file
+
+                    print(line, file=dupe_file)
+                    continue
+                else:
+                    keys.add(key)
+            else:
+                current_timestamp = line_timestamp
+                keys = set()
+                keys.add(key)
+
+            print(line, end='')
+            out_count += 1
+            if out_count % 100000 == 0:
+                print(f"in {in_count:012d} docs, out {out_count:012d} docs, errors {error_count:012d}", file=sys.stderr)
+        except Exception as e:
+            raise Exception(f"Error processing {line}") from e
+        
+    for dupe_file in dupe_files:
+        dupe_files[dupe_file].close()

02a949c22a37a9a53657dd2447f78501edc9341d
Domenico Andreoli <domenico.andreoli@elastic.co>
2022-07-13 17:30:05 +0200

    Download assets

--- elastic/endpoint/track_processors/assets_loader.py
+++ elastic/endpoint/track_processors/assets_loader.py
@@ -15,14 +15,78 @@
 # specific language governing permissions and limitations
 # under the License.
 
+import os
+import json
 import logging
+
+from urllib.parse import urlparse
 
 logger = logging.getLogger(__name__)
 
 
+def load_index_template(track, asset_content):
+    pass
+
+
+def load_component_template(track, asset_content):
+    pass
+
+
+def load_ingest_pipeline(track, asset_content):
+    pass
+
+
+def load_ilm_policy(track, asset_content):
+    pass
+
+
+asset_loaders = {
+    "component_templates": load_component_template,
+    "index_templates": load_index_template,
+    "ingest_pipelines": load_ingest_pipeline,
+    "ilm_policies": load_ilm_policy,
+}
+
+
+def load_from_github(track, packages, repo_path):
+    from elastic.package import assets
+    from github import Github
+
+    if packages:
+        github = Github(os.getenv("ASSETS_AUTH_TOKEN") or None)
+        repo = github.get_repo(repo_path)
+
+    for package in packages:
+        logger.info(f"Downloading assets of [{package}] from [{repo.html_url}]")
+        entries = assets.get_remote_assets(package, repo)
+
+        count = 0
+        for path, content in assets.download_assets(entries):
+            path_parts = os.path.split(path[len(package) + 1:])
+
+            if not path_parts[0]:
+                continue
+            if path_parts[0] in asset_loaders:
+                asset_loaders[path_parts[0]](track, json.loads(content))
+                count += 1
+            else:
+                logger.warning(f"Skipping unknown asset type: {path_parts[0]}")
+
+        logger.info(f"Loaded [{count}] assets")
+
+
 class AssetsLoader:
     def on_after_load_track(self, track):
-        from elastic.package import assets
+
+        params = track.selected_challenge_or_default.parameters.get("assets", {})
+        repository = params.get("repository", "https://github.com/elastic/package-assets")
+        packages = params.get("packages", [])
+
+        repo_parts = urlparse(repository)
+        if repo_parts.scheme.startswith("http") and repo_parts.netloc == "github.com":
+            load_from_github(track, packages, repo_parts.path[1:])
+        else:
+            raise ValueError(f"Unsupported repository: {repository}")
 
     def on_prepare_track(self, track, data_root_dir):
         return []
--- elastic/tests/track_processors/assets_loader_test.py
+++ elastic/tests/track_processors/assets_loader_test.py
@@ -26,6 +26,22 @@ def assets_loader():
     return AssetsLoader()
 
 
+@pytest.fixture
+def parameters():
+    return {
+        "assets": {
+            "packages": [
+                "endpoint/8.3.0"
+            ]
+        }
+    }
+
+
+@pytest.fixture
+def track(parameters):
+    return EmptyTrack(parameters=parameters)
+
+
 def test_no_assets(assets_loader):
     track = EmptyTrack()
     assets_loader.on_after_load_track(track)
@@ -61,3 +77,39 @@ def test_empty_packages(assets_loader):
     assert track.data_streams == []
     assert track.component_templates == []
     assert track.composable_templates == []
+
+
+def test_invalid_packages(assets_loader):
+    parameters = {
+        "assets": {
+            "packages": [
+                "invalid/a.b.c"
+            ]
+        }
+    }
+    track = EmptyTrack(parameters=parameters)
+    with pytest.raises(ValueError) as exc:
+        assets_loader.on_after_load_track(track)
+    assert str(exc.value) == f"Package not found: {parameters['assets']['packages'][0]}"
+
+
+def test_invalid_repo(assets_loader):
+    for repo in ["git@github.com:elastic/package-assets", "https://gitlab.com/elastic/package-assets"]:
+        parameters = {
+            "assets": {
+                "repository": repo
+            }
+        }
+        track = EmptyTrack(parameters=parameters)
+
+        with pytest.raises(ValueError) as exc:
+            assets_loader.on_after_load_track(track)
+        assert str(exc.value) == f"Unsupported repository: {repo}"
+
+        assert track.data_streams == []
+        assert track.component_templates == []
+        assert track.composable_templates == []
+
+
+def test_valid_packages(assets_loader, track):
+    assets_loader.on_after_load_track(track)

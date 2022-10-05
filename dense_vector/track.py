6b668516deea30de78e36b0e67f5b1d10bb3a7f4
Rick Boyd <richard.boyd@elastic.co>
2022-10-05 13:49:32 -0400

    [Dense Vector] Move to _search API and add random query vector support (#330)
    This PR moves KNN searches from raw-request to search operations, and adds functionality to allow to use random query vectors per iteration instead of needing to provide a static query vector for a given task.

---
+++ dense_vector/track.py
@@ -0,0 +1,32 @@
+import random
+
+
+def knn_param_source(track, params, **kwargs):
+    # ensure the sequence of vector queries is the same from run to run
+    random.seed(31)
+    # choose a suitable index: if there is only one defined for this track
+    # choose that one, but let the user always override index
+    if len(track.indices) == 1:
+        default_index = track.indices[0].name
+    else:
+        default_index = "_all"
+
+    index_name = params.get("index", default_index)
+
+    return {
+        "body": {
+            "knn": {
+                "field": "vector",
+                "query_vector": params.get("query-vector", [random.uniform(-1.0, 1.0) for _ in range(96)]),
+                "k": params.get("k", 10),
+                "num_candidates": params.get("num-candidates", 100),
+            },
+            "_source": False,
+        },
+        "index": index_name,
+        "cache": params.get("cache", False),
+    }
+
+
+def register(registry):
+    registry.register_param_source("knn-param-source", knn_param_source)

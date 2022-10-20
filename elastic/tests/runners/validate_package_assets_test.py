65c77d5a8ccbdc98637a3bebc52fb61e88c333a4
Mike Baamonde <mike.baamonde@elastic.co>
2022-10-20 13:12:38 -0400

    Add a happy-path unit test.

--- elastic/tests/runners/validate_package_assets_test.py
+++ elastic/tests/runners/validate_package_assets_test.py
@@ -24,7 +24,7 @@ from tests import as_future
 
 @mock.patch("elasticsearch.Elasticsearch")
 @pytest.mark.asyncio
-async def test_validate_package_assets(es):
+async def test_validate_package_assets_raises_exception_if_missing_packages(es):
     installed = ["apache", "kafka", "mysql", "nginx", "postgresql", "redis"]
     missing = ["system"]
     templates = {"index_templates": [{"index_template": {"_meta": {"package": {"name": p}}}} for p in installed]}
@@ -35,3 +35,14 @@ async def test_validate_package_assets(es):
         await validate_package_assets(es, params)
 
     assert f"Index templates missing for packages: {missing}" in str(e)
+
+
+@mock.patch("elasticsearch.Elasticsearch")
+@pytest.mark.asyncio
+async def test_validate_package_assets_with_all_packages_installed(es):
+    installed = ["apache", "kafka", "mysql", "nginx", "postgresql", "redis", "system"]
+    templates = {"index_templates": [{"index_template": {"_meta": {"package": {"name": p}}}} for p in installed]}
+    es.indices.get_index_template.return_value = as_future(templates)
+
+    params = {"packages": ["apache", "kafka", "mysql", "nginx", "postgresql", "redis", "system"], "asset-types": "index-templates"}
+    assert await validate_package_assets(es, params)

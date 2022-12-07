6a842e74d8d9376810eb1f68ef957274f2abdd36
Jack Conradson <osjdconrad@gmail.com>
2022-12-07 14:32:10 -0800

    update based on pr feedback

--- dense_vector/track.py
+++ dense_vector/track.py
@@ -1,6 +1,7 @@
 import json
 import os
 
+
 class KnnParamSource:
     def __init__(self, track, params, **kwargs):
         # choose a suitable index: if there is only one defined for this track
@@ -54,6 +55,7 @@ class KnnParamSource:
         self._iters += 1
         return result
 
+
 # For each query this will generate both the knn query and the equivalent
 # score script query. The two queries can then be executed and used
 # to gauge the accuracy of the knn query.
@@ -72,69 +74,73 @@ class KnnRecallParamSource:
         with open(os.path.join(cwd, "queries.json"), "r") as file:
             lines = file.readlines()
         self._queries = [json.loads(line) for line in lines]
-        self._iters = 0
         self.infinite = True
 
     def partition(self, partition_index, total_partitions):
         return self
 
     def params(self):
-        result = {"index": self._index_name, "cache": self._params.get("cache", False), "size": self._params.get("k", 10)}
-        result["script_body"] = {
-            "query": {
-                "script_score": {
-                    "query": {"match_all": {}},
-                    "script": {
-                        "source": "cosineSimilarity(params.query, 'vector') + 1.0",
-                        "params": {"query": self._queries[self._iters]},
-                    },
-                }
-            },
-            "_source": False,
+        return {
+            "index": self._index_name,
+            "cache": self._params.get("cache", False),
+            "size": self._params.get("k", 10),
+            "num_candidates": self._params.get("num-candidates", 100),
+            "queries": self._queries,
         }
-        result["knn_body"] = {
-            "knn": {
-                "field": "vector",
-                "query_vector": self._queries[self._iters],
-                "k": self._params.get("k", 10),
-                "num_candidates": self._params.get("num-candidates", 100),
-            },
-            "_source": False,
-        }
-        self._iters += 1
-        return result
+
 
 # Used in tandem with the KnnRecallParamSource. This executes both a knn query
 # and an equivalent score script query. Results are then compared to gauge
 # the accuracy of the knn query.
 class KnnRecallRunner:
     async def __call__(self, es, params):
-        knn_result = await es.search(
-            body=params["knn_body"], 
-            index=params["index"], 
-            request_cache=params["cache"], 
-            size=params["size"]
-        )
-        script_result = await es.search(
-            body=params["script_body"], 
-            index=params["index"], 
-            request_cache=params["cache"], 
-            size=params["size"]
-        )
-        knn_hits = {hit["_id"] for hit in knn_result["hits"]["hits"]}
-        script_hits = {hit["_id"] for hit in script_result["hits"]["hits"]}
-        intersection = knn_hits.intersection(script_hits)
-        return {
-            "k": params["knn_body"]["knn"]["k"],
-            "num_candidates": params["knn_body"]["knn"]["num_candidates"],
-            "recall": len(intersection)
-        }
+        intersection = 0
+        total = 0
+
+        for query in params["queries"]:
+            knn_result = await es.search(
+                body={
+                    "knn": {
+                        "field": "vector",
+                        "query_vector": query,
+                        "k": params["size"],
+                        "num_candidates": params["num_candidates"],
+                    },
+                    "_source": False,
+                },
+                index=params["index"],
+                request_cache=params["cache"],
+                size=params["size"],
+            )
+            script_result = await es.search(
+                body={
+                    "query": {
+                        "script_score": {
+                            "query": {"match_all": {}},
+                            "script": {
+                                "source": "cosineSimilarity(params.query, 'vector') + 1.0",
+                                "params": {"query": query},
+                            },
+                        }
+                    },
+                    "_source": False,
+                },
+                index=params["index"],
+                request_cache=params["cache"],
+                size=params["size"],
+            )
+            knn_hits = {hit["_id"] for hit in knn_result["hits"]["hits"]}
+            script_hits = {hit["_id"] for hit in script_result["hits"]["hits"]}
+            intersection += len(knn_hits.intersection(script_hits))
+            total += len(script_hits)
+
+        return {"recall": intersection / total}
 
     def __repr__(self, *args, **kwargs):
         return "knn-recall"
+
 
 def register(registry):
     registry.register_param_source("knn-param-source", KnnParamSource)
     registry.register_param_source("knn-recall-param-source", KnnRecallParamSource)
     registry.register_runner("knn-recall", KnnRecallRunner(), async_runner=True)
-

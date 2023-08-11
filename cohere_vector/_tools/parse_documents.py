60235b17155aae6342411999e9354cb03d76887e
Rodney Norris <rodney.norris@elastic.co>
2023-08-11 18:26:43 +0000

    cohere_vector: split dataset into 11 files
    Updated the parse documents script to output multiple json files instead
    of one large file.

--- cohere_vector/_tools/parse_documents.py
+++ cohere_vector/_tools/parse_documents.py
@@ -2,14 +2,16 @@ import json
 import sys
 
 import numpy as np
-from datasets import load_dataset
+from datasets import load_dataset, DownloadMode
 
 DATASET_NAME: str = f"Cohere/miracl-en-corpus-22-12"
-OUTPUT_FILENAME: str = "cohere-documents.json"
+DATASET_DL_PROCS: int = 6
+OUTPUT_FILENAME: str = "cohere-documents"
 DEFAULT_MAX_DOCS = -1
 TOTAL_DOCS = 32893221
+MAX_DOCS_PER_FILE = 3_000_000
+TOTAL_PAGES = 11
 PROGRESS_EVERY = 100
-
 
 def progress_bar(count, total):
     bar_length = 100
@@ -19,18 +21,30 @@ def progress_bar(count, total):
     sys.stdout.write("[{}] {}{} ... {:,}/{:,}\r".format(bar, percentage, "%", count, total))
     sys.stdout.flush()
 
+def output_pages(start_page, end_page):
+    for page in range(start_page, end_page + 1):
+        start_index = (page - 1) * MAX_DOCS_PER_FILE
+        end_index = start_index + MAX_DOCS_PER_FILE
+        if end_index > TOTAL_DOCS:
+            end_index = TOTAL_DOCS
+        output_filename = f"{OUTPUT_FILENAME}-{page:02d}.json"
+        print(f"Outputing page {page} documents to {output_filename}")
+        with open(output_filename, "w") as documents_file:
+            output_documents(documents_file, start_index, end_index)
 
-def output_documents(docs_file):
-    max_documents = int(sys.argv[1]) if len(sys.argv) >= 2 else DEFAULT_MAX_DOCS
-    partial_index = max_documents != DEFAULT_MAX_DOCS
 
-    if partial_index:
-        print("Parsing {} documents".format(max_documents))
-    else:
-        print("Parsing entire {} dataset".format(DATASET_NAME))
-    docs = load_dataset(DATASET_NAME, split="train")
+def output_documents(docs_file, start_index, end_index):
     doc_count = 0
-    progress_bar(doc_count, max_documents if partial_index else TOTAL_DOCS)
+    dataset_size = end_index - start_index
+    print(f"Parsing {dataset_size} documents from {DATASET_NAME} [{start_index}:{end_index}]")
+    docs = load_dataset(
+        DATASET_NAME,
+        split=f"train[{start_index}:{end_index}]",
+        num_proc=DATASET_DL_PROCS,
+        download_mode=DownloadMode.REUSE_DATASET_IF_EXISTS,
+    )
+
+    progress_bar(doc_count, dataset_size)
     for doc in docs:
         v = np.array(doc["emb"])
         v_unit = v / np.linalg.norm(v)
@@ -48,13 +62,22 @@ def output_documents(docs_file):
         docs_file.write("\n")
         doc_count += 1
         if doc_count % PROGRESS_EVERY == 0:
-            progress_bar(doc_count, max_documents if partial_index else TOTAL_DOCS)
+            progress_bar(doc_count, dataset_size)
+    print(f"Wrote {doc_count} documents to output file.")
 
-        if partial_index and doc_count >= max_documents:
-            return
+def parse_arguments():
+    if len(sys.argv) >= 3:
+        return (DEFAULT_MAX_DOCS, int(sys.argv[1]), int(sys.argv[2]))
 
+    if len(sys.argv) >= 2:
+        return (int(sys.argv[1]), 1, TOTAL_PAGES)
+    return (DEFAULT_MAX_DOCS, 1, TOTAL_PAGES)
 
 if __name__ == "__main__":
-    print("Outputing documents to {}".format(OUTPUT_FILENAME))
-    with open(OUTPUT_FILENAME, "w") as documents_file:
-        output_documents(documents_file)
+    (max_documents, start_page, end_page) = parse_arguments()
+    if max_documents == DEFAULT_MAX_DOCS:
+        output_pages(start_page, end_page)
+    else:
+        print("Outputing documents to {}.json".format(OUTPUT_FILENAME))
+        with open(f"{OUTPUT_FILENAME}.json", "w") as documents_file:
+            output_documents(documents_file, 0, max_documents)

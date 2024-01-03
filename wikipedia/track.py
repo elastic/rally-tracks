8b7c5018014c9caf503957a0971267c0cfcc1302
Wes Mason <wes@1stvamp.org>
2024-01-03 16:14:07 +0000

    tuple not list

--- wikipedia/track.py
+++ wikipedia/track.py
@@ -246,7 +246,8 @@ async def create_users_and_roles(es, params):
 
     for n, roles in batched(ROLE_IDS, int(len(ROLE_IDS)/10)):
         await es.security.put_user(
-            username=USERS[n]["username"], params={"roles": roles + ['app-admin#']}
+            username=USERS[n]["username"], params={"roles": roles +
+                ('app-admin#')}
         )
 
     #await es.indices.refresh(index="wikipedia")

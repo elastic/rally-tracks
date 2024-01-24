ade7cefa8d3c3df6b603f631989392a62cae594c
Wes Mason <wes@1stvamp.org>
2024-01-24 17:35:04 +0000

    remove cache param

--- wikipedia/track.py
+++ wikipedia/track.py
@@ -275,7 +275,6 @@ async def create_users_and_roles(es, params):
                         }
                     }
                 },
-            cache=False
         )
 
     await es.indices.refresh(index="wikipedia")

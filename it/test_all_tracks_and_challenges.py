009ad300bb8683022156de8c357f088a4bd0e539
kyungeunni <kyungeunni@gmail.com>
2024-02-22 13:50:19 +0900

    chore: include apm test

--- it/test_all_tracks_and_challenges.py
+++ it/test_all_tracks_and_challenges.py
@@ -21,7 +21,7 @@ pytest_rally = pytest.importorskip("pytest_rally")
 
 
 class TestTrackRepository:
-    skip_tracks = ["elastic/apm", "elastic/logs", "elastic/security", "k8s_metrics", "sql", "elser-ingest-speedtest"]
+    skip_tracks = ["elastic/logs", "elastic/security", "k8s_metrics", "sql", "elser-ingest-speedtest"]
     disable_assertions = {
         "http_logs": ["append-no-conflicts", "runtime-fields"],
         "nyc_taxis": ["update-aggs-only"],

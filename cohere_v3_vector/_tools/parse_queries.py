7891ec1a7ddb82e972f4170c8c15e9089d11879a
Wes Mason <wes@1stvamp.org>
2024-03-04 16:35:59 +0000

    msmarco-v2-vector: lint fixes

--- cohere_v3_vector/_tools/parse_queries.py
+++ cohere_v3_vector/_tools/parse_queries.py
@@ -4,13 +4,14 @@ from os import environ
 import cohere
 from datasets import load_dataset
 
-DATASET_NAME: str = 'Cohere/msmarco-v2-embed-english-v3'
-DATASET_SPLIT: str = 'train'
-OUTPUT_FILENAME: str = 'queries.json'
+DATASET_NAME: str = "Cohere/msmarco-v2-embed-english-v3"
+DATASET_SPLIT: str = "train"
+OUTPUT_FILENAME: str = "queries.json"
 MAX_DOCS = 12_000
 
 # Get your production Cohere API key from https://dashboard.cohere.com/api-keys
-co = cohere.Client(environ['COHERE_API_KEY'])
+co = cohere.Client(environ["COHERE_API_KEY"])
+
 
 def output_queries(queries_file):
     queries = load_dataset(DATASET_NAME, split=DATASET_SPLIT, streaming=True)
@@ -20,13 +21,12 @@ def output_queries(queries_file):
         if queries_count > MAX_DOCS:
             break
 
-        response = co.embed(texts=[query['text']], model='embed-english-v3.0',
-                            input_type='search_query')
+        response = co.embed(texts=[query["text"]], model="embed-english-v3.0", input_type="search_query")
         queries_file.write(json.dumps(response.embeddings))
         queries_file.write("\n")
         queries_count += 1
 
 
-if __name__ == '__main__':
-    with open(OUTPUT_FILENAME, 'w') as queries_file:
+if __name__ == "__main__":
+    with open(OUTPUT_FILENAME, "w") as queries_file:
         output_queries(queries_file)

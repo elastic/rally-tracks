981a5291649acacceecd42e2742d24812fb72665
Kathleen DeRusso <kathleen.derusso@elastic.co>
2024-10-17 17:01:55 -0400

    Add performance metrics

--- search/mteb/dbpedia/track.py
+++ search/mteb/dbpedia/track.py
@@ -1,11 +1,14 @@
 import csv
 import json
+import logging
 import os
 from collections import defaultdict
 from typing import Dict
 
 Qrels = Dict[str, Dict[str, int]]
 Results = Dict[str, Dict[str, float]]
+
+logger = logging.getLogger(__name__)
 
 
 def calc_ndcg(qrels: Qrels, results: Results, k_list: list):
@@ -53,7 +56,65 @@ def generate_bm25_query(text_field, query, boost=1.0):
     return {"query": {"match": {f"{text_field}": {"query": query, "boost": boost}}}}
 
 
+def generate_query(query_string, title_field, title_boost, text_field, text_boost):
+    return {
+        "query": {
+            "multi_match": {
+                "minimum_should_match": "1<-1 3<49%",
+                "type": "best_fields",
+                "fuzziness": "AUTO",
+                "prefix_length": 2,
+                "query": query_string,
+                "fields": [f"{title_field}^{title_boost}", f"{text_field}^{text_boost}"],
+            }
+        }
+    }
+
+
 class QueryParamsSource:
+    def __init__(self, track, params, **kwargs):
+        # choose a suitable index: if there is only one defined for this track
+        # choose that one, but let the user always override index
+        if len(track.indices) == 1:
+            default_index = track.indices[0].name
+        else:
+            default_index = "_all"
+
+        self._index_name = params.get("index", default_index)
+        self._cache = params.get("cache", False)
+        self._size = params.get("size", 10)
+        self._title_field = params.get("title_field", "title")
+        self._text_field = params.get("text_field", "text")
+        self._title_boost = params.get("title_boost", 5)
+        self._text_boost = params.get("text_boost", 1)
+        self._query_file = params.get("query_source", "queries.json")
+        self._track_total_hits = params.get("track_total_hits", False)
+        self._params = params
+        self.infinite = True
+
+        cwd = os.path.dirname(__file__)
+        with open(os.path.join(cwd, self._query_file), "r") as file:
+            self._queries = json.load(file)
+        self._iters = 0
+
+    def partition(self, partition_index, total_partitions):
+        return self
+
+    def params(self):
+        query_obj = self._queries[self._iters]
+        query = generate_query(query_obj["text"], self._title_field, self._title_boost, self._text_field, self._text_boost)
+
+        self._iters = (self._iters + 1) % len(self._queries)
+        return {
+            "index": self._index_name,
+            "cache": self._cache,
+            "size": self._size,
+            "track_total_hits": self._track_total_hits,
+            "body": query,
+        }
+
+
+class RelevanceParamsSource:
     def __init__(self, track, params, **kwargs):
         # choose a suitable index: if there is only one defined for this track
         # choose that one, but let the user always override index
@@ -108,19 +169,9 @@ class TextSearchRelevanceRunner:
         for query in params["queries"]:
             query_id = query["_id"]
 
-            text_search_query = {
-                "query": {
-                    "multi_match": {
-                        "minimum_should_match": "1<-1 3<49%",
-                        "type": "best_fields",
-                        "fuzziness": "AUTO",
-                        "prefix_length": 2,
-                        "query": query["text"],
-                        "fields": [f"{params['title_field']}^{params['title_boost']}", f"{params['text_field']}^{params['text_boost']}"],
-                    }
-                }
-            }
-
+            text_search_query = generate_query(
+                query["text"], params["title_field"], params["title_boost"], params["text_field"], params["text_boost"]
+            )
             text_search_result = await es.search(
                 body=text_search_query,
                 index=params["index"],
@@ -136,8 +187,8 @@ class TextSearchRelevanceRunner:
 
         text_search_relevance = calc_ndcg(params["qrels"], text_search_results, [10, 100])
 
-        print(
-            f'text_search_relevance_10 = {text_search_relevance["ndcg_cut@10"]}, text_search_relevance_100 = {text_search_relevance["ndcg_cut@100"]}\n'
+        logger.debug(
+            f'text_search_relevance_10 = {text_search_relevance["ndcg_cut@10"]}, text_search_relevance_100 = {text_search_relevance["ndcg_cut@100"]}'
         )
 
         return {
@@ -151,4 +202,5 @@ class TextSearchRelevanceRunner:
 
 def register(registry):
     registry.register_param_source("query_param_source", QueryParamsSource)
+    registry.register_param_source("relevance_param_source", RelevanceParamsSource)
     registry.register_runner("text_search_relevance", TextSearchRelevanceRunner(), async_runner=True)

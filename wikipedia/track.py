8b049c4e571e2e33f6b3ca1410c95ad7d2714a35
carlosdelest <carlos.delgado@elastic.co>
2025-04-01 17:16:08 +0200

    Fix size again

--- wikipedia/track.py
+++ wikipedia/track.py
@@ -220,7 +220,7 @@ class EsqlSearchParamSource(QueryIteratorParamSource):
         super().__init__(track, params, **kwargs)
         self._index_name = params.get("index", track.indices[0].name if len(track.indices) == 1 else "_all")
         self._search_fields = self._params["search-fields"]
-        self._size = params.get("size", 10)
+        self._size = params.get("size", 20)
         self._query_type = self._params["query-type"]
 
     def params(self):
@@ -251,7 +251,7 @@ class QueryParamSource(QueryIteratorParamSource):
         super().__init__(track, params, **kwargs)
         self._index_name = params.get("index", track.indices[0].name if len(track.indices) == 1 else "_all")
         self._cache = params.get("cache", False)
-        self._size = params.get("size", 10)
+        self._size = params.get("size")
         self._query_type = self._params["query-type"]
 
     def params(self):
@@ -276,7 +276,7 @@ class QueryParamSource(QueryIteratorParamSource):
             "body": {
                 "_source": {"includes": ["title"]},
                 "query": query_body,
-                "size": self._params["size"],
+                "size": self._size,
             },
             "index": self._index_name,
             "cache": self._cache,

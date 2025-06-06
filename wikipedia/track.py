7a4b385a433621ceb011b61bddc49d5ec2e866f7
Kathleen DeRusso <kathleen.derusso@elastic.co>
2025-06-06 10:52:25 -0400

    Add match phrase tests to Wikipedia track (#782)
    * Add match phrase tests to Wikipedia track
    
    * Fix typo
    
    * Add match_phrase support to track.py
    
    * PR feedback

--- wikipedia/track.py
+++ wikipedia/track.py
@@ -234,6 +234,8 @@ class EsqlSearchParamSource(QueryIteratorParamSource):
                 query_body = f'KQL("{ self._search_fields }:{ query }")'
             elif self._query_type == "term":
                 query_body = f'TERM(title, "{ query }") OR TERM(content, "{ query }")'
+            elif self._query_type == "match_phrase":
+                query_body = f'MATCH_PHRASE(title, "{ query }") OR MATCH_PHRASE(content, "{ query }")'
             else:
                 raise ValueError("Unknown query type: " + self._query_type)
 
@@ -261,11 +263,13 @@ class QueryParamSource(QueryIteratorParamSource):
             elif self._query_type == "kql":
                 query_body = {"kql": {"query": f'{ self._params["search-fields"] }:"{ query }"'}}
             elif self._query_type == "match":
-                query_body = {"match": {"content": query}}
+                query_body = {"bool": {"should": [{"match": {"title": query}}, {"match": {"content": query}}]}}
             elif self._query_type == "multi_match":
                 query_body = {"bool": {"should": [{"match": {"title": query}}, {"match": {"content": query}}]}}
             elif self._query_type == "term":
                 query_body = {"bool": {"should": [{"term": {"title": query}}, {"term": {"content": query}}]}}
+            elif self._query_type == "match_phrase":
+                query_body = {"bool": {"should": [{"match_phrase": {"title": query}}, {"match_phrase": {"content": query}}]}}
             else:
                 raise ValueError("Unknown query type: " + self._query_type)
 

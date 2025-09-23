bd301b53ce0cb1b573666d52f882cb0e24419a3b
NickDris <nick.dris@elastic.co>
2025-09-23 18:57:07 +0300

    Fix track filter argument to serverless tests

--- .buildkite/it/run_serverless.sh
+++ .buildkite/it/run_serverless.sh
@@ -42,19 +42,18 @@ IFS=',' read -ra changed_dirs_arr <<< "$CHANGED_TOP_LEVEL_DIRS"
 
 all_changed_arr=("${changed_files_arr[@]}" "${changed_dirs_arr[@]}")
 
-TRACK_FILTER="--track-filter=${CHANGED_TOP_LEVEL_DIRS}"
+TRACK_FILTER_ARG="--track-filter=${CHANGED_TOP_LEVEL_DIRS}"
 
 # If any changes match one of the RUN_FULL_CI_WHEN_CHANGED paths, run full CI
 for static_path in "${RUN_FULL_CI_WHEN_CHANGED[@]}"; do
     for changed in "${all_changed_arr[@]}"; do
         if [[ "$static_path" == "$changed" ]]; then
             echo "Matched '$static_path' in changed files/dirs. Running full CI."
-            TRACK_FILTER=""
+            TRACK_FILTER_ARG=""
             break 2
         fi
     done
 done
-echo "--- Run IT serverless test \"$TEST_NAME\" with track filter \"$TRACK_FILTER\" :pytest:"
+echo "--- Run IT serverless test \"$TEST_NAME\" $TRACK_FILTER_ARG :pytest:"
 
-hatch -v -e it_serverless run $TEST_NAME --track-filter="$TRACK_FILTER"
-
+hatch -v -e it_serverless run $TEST_NAME $TRACK_FILTER_ARG

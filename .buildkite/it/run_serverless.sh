d3c3501acc641cac5ee8985d8af0ab795e457985
esbenchmachine <116851159+esbenchmachine@users.noreply.github.com>
2025-10-08 17:18:52 +0300

    Tests tracks selectively based on PR changes (#858) (#881)
    Implements test execution based on PR changes by integrating
    pytest-rally plugin's --track-filter option. Tests will now only run
    for tracks affected by changes in the top-level directories of the
    repository.
    
    - Updates test command configurations to support dynamic track
    filtering arguments
    - Adds pytest track markers to associate test classes and methods
     with specific tracks
    - Implements CI workflow changes to detect changed tracks and
    pass them as filter arguments
    - Defines RUN_FULL_CI_WHEN_CHANGED env variable which
    point to files that when changed full CI is executed.
    
    (cherry picked from commit 714442df274e75a659fe104f0b634f70f55f4c58)
    
    Co-authored-by: Nick Dris <nick.dris@elastic.co>

--- .buildkite/it/run_serverless.sh
+++ .buildkite/it/run_serverless.sh
@@ -14,6 +14,7 @@ echo "\$nrconf{restart} = 'a';" | sudo tee -a /etc/needrestart/needrestart.conf 
 
 PYTHON_VERSION="$1"
 TEST_NAME="$2"
+IFS=',' read -ra RUN_FULL_CI_WHEN_CHANGED <<< "$3"
 
 echo "--- System dependencies"
 
@@ -29,7 +30,29 @@ echo "--- Python modules"
 source .venv/bin/activate
 python -m pip install .[develop]
 
-echo "--- Run IT serverless test \"$TEST_NAME\" :pytest:"
+echo "--- Track filter modification"
 
-hatch -v -e it_serverless run $TEST_NAME
+CHANGED_FILES=$(git diff --name-only origin/master...HEAD)
+readarray -t changed_files_arr <<< "$CHANGED_FILES"
 
+CHANGED_TOP_LEVEL_DIRS=$(echo "$CHANGED_FILES" | grep '/' | awk -F/ '{print $1}' | sort -u | paste -sd, -)
+CHANGED_TOP_LEVEL_DIRS=${CHANGED_TOP_LEVEL_DIRS%,}
+IFS=',' read -ra changed_dirs_arr <<< "$CHANGED_TOP_LEVEL_DIRS"
+
+all_changed_arr=("${changed_files_arr[@]}" "${changed_dirs_arr[@]}")
+
+TRACK_FILTER_ARG="--track-filter=${CHANGED_TOP_LEVEL_DIRS}"
+
+# If any changes match one of the RUN_FULL_CI_WHEN_CHANGED paths, run full CI
+for static_path in "${RUN_FULL_CI_WHEN_CHANGED[@]}"; do
+    for changed in "${all_changed_arr[@]}"; do
+        if [[ "$static_path" == "$changed" ]]; then
+            echo "Matched '$static_path' in changed files/dirs. Running full CI."
+            TRACK_FILTER_ARG=""
+            break 2
+        fi
+    done
+done
+echo "--- Run IT serverless test \"$TEST_NAME\" $TRACK_FILTER_ARG :pytest:"
+
+hatch -v -e it_serverless run $TEST_NAME $TRACK_FILTER_ARG
